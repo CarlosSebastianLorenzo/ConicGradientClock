35a7c8d7208850818bfe388d1d36e11667332731
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-05-02 20:38:43 -0300

    estyles ul

--- script.js
+++ script.js
@@ -18,16 +18,13 @@ const clock = document.getElementById("clock");
 const root = document.documentElement;
 let hourAngle = `${hour*(360/12)}deg`
 let minuteAngle = `${minute*(360/60)-hour*(360/12)}deg`
-    
 
 root.style.setProperty('--secondHandDegree', `${second*(360/60)-90}deg`)
 root.style.setProperty('--minuteHandDegree', `${minute*(360/60)-90}deg`)
 root.style.setProperty('--hourHandDegree', `${hour*(360/12)-90}deg`)
 clock.style.background= `conic-gradient(from ${hourAngle},var(--primary),var(--dark)${minuteAngle},var(--primary)0deg,var(--dark))`;
 
-
 setInterval(function(){
-
     today = new Date();
     hour = today.getHours()>12?today.getHours()-12:today.getHours();
     minute = today.getMinutes();
@@ -36,7 +33,6 @@ setInterval(function(){
     minuteAngle = `${minute*(360/60)-hour*(360/12)}deg`
     console.log(hour, hourAngle, minute, minuteAngle)
     clock.style.background= `conic-gradient(from ${hourAngle},var(--primary),var(--dark)${minuteAngle},var(--primary)0deg,var(--dark))`;
-    
 }
     ,60000
 )
