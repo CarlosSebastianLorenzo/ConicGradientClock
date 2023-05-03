75c5f17a4adcbc5b760da09da93457a34278d7c4
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-05-02 21:33:25 -0300

    fix gradient

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-function handRotate(e){
-    e.target.animate([
-        {rotate: "360deg"}
-    ],{
-        easing: "linear"
-    }
-    )
-}
-
 let today = new Date();
 let hour = today.getHours()>12?today.getHours()-12:today.getHours();
 let minute = today.getMinutes();
 let second = today.getSeconds();
-const hourHand = document.getElementsByClassName("hour-hand")[0];
-const minuteHand = document.getElementsByClassName("minute-hand")[0];
-const secondHand = document.getElementsByClassName("second-hand")[0];
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
 const clock = document.getElementById("clock");
 const root = document.documentElement;
-let hourAngle = `${hour*(360/12)}deg`
-let minuteAngle = `${minute*(360/60)-hour*(360/12)}deg`
+let hourAngle = `${hour*(360/12)+(minute*(360/60))/360+(second*(360/60))/360}deg`
+let minuteAngle = `${minute*(360/60)+hour*(360/12)/360+90+(second*(360/60))/360}deg`
 
 root.style.setProperty('--secondHandDegree', `${second*(360/60)-90}deg`)
 root.style.setProperty('--minuteHandDegree', `${minute*(360/60)-90}deg`)
@@ -29,10 +20,9 @@ setInterval(function(){
     hour = today.getHours()>12?today.getHours()-12:today.getHours();
     minute = today.getMinutes();
     second = today.getSeconds();
-    hourAngle = `${hour*(360/12)}deg`
-    minuteAngle = `${minute*(360/60)-hour*(360/12)}deg`
-    console.log(hour, hourAngle, minute, minuteAngle)
+    hourAngle = `${hour*(360/12)+(minute*(360/60))/360+(second*(360/60))/360}deg`
+    minuteAngle = `${minute*(360/60)+hour*(360/12)/360+90+(second*(360/60))/360}deg`
     clock.style.background= `conic-gradient(from ${hourAngle},var(--primary),var(--dark)${minuteAngle},var(--primary)0deg,var(--dark))`;
 }
-    ,60000
+    ,1000
 )
